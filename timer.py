e4c3bd8d6aae219a898638cb1dc574be73b78be5
BIPANCHISARMA <95870329+BIPANCHISARMA@users.noreply.github.com>
2023-01-03 16:51:00 +0530

    Add files via upload

---
+++ timer.py
@@ -0,0 +1,13 @@
+import time
+from playsound import playsound
+def countdown_timer(seconds):
+    while seconds > 0:
+        mins = int(seconds / 60)
+        secs = int(seconds % 60)
+        timer = f'{mins}:{secs}'
+        print(timer)
+        seconds -= 1
+    print('Time Up!')
+    playsound('C:/Users/csarm/Downloads/class110/mixkit.wav')
+seconds = input("Enter the time in number of seconds: ")
+countdown_timer(int(seconds))
